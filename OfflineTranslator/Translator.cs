dc14181a2eea4a1354401d61fb03679834b98a42
KilllKa <113077885+KilllKa@users.noreply.github.com>
2024-11-29 02:42:10 +0700

    Add transliteration for unknowned words

--- OfflineTranslator/Translator.cs
+++ OfflineTranslator/Translator.cs
@@ -33,15 +33,48 @@ namespace OfflineTranslator
             File.WriteAllText(filePath, json);
         }
 
+        private string Transliterate(string word)
+        {
+            var transliterationMap = new Dictionary<char, string>
+            {
+                {'а', "a"}, {'б', "b"}, {'в', "v"}, {'г', "g"}, {'д', "d"},
+                {'е', "e"}, {'ё', "yo"}, {'ж', "zh"}, {'з', "z"}, {'и', "i"},
+                {'й', "y"}, {'к', "k"}, {'л', "l"}, {'м', "m"}, {'н', "n"},
+                {'о', "o"}, {'п', "p"}, {'р', "r"}, {'с', "s"}, {'т', "t"},
+                {'у', "u"}, {'ф', "f"}, {'х', "kh"}, {'ц', "ts"}, {'ч', "ch"},
+                {'ш', "sh"}, {'щ', "shch"}, {'ъ', ""}, {'ы', "y"}, {'ь', ""},
+                {'э', "e"}, {'ю', "yu"}, {'я', "ya"}
+            };
+
+            var result = "";
+            foreach (var ch in word)
+            {
+                if (transliterationMap.TryGetValue(char.ToLower(ch), out var translitChar))
+                {
+                    result += char.IsUpper(ch) ? 
+                        char.ToUpper(translitChar[0]) + translitChar.Substring(1) : translitChar;
+                }
+                else
+                {
+                    result += ch;
+                }
+            }
+            return result;
+        }
+        
         public string TranslateText(string text)
         {
-            var words = text.Split(' '); //StringSplitOptions.RemoveEmptyEntries
+            var words = text.Split(new [] {' ', '\n', '\t', ',', '.', '!', '?'}, StringSplitOptions.RemoveEmptyEntries);
             for (int i = 0; i < words.Length; i++)
             {
                 var word = words[i].ToLower();
                 if (_dictionary.TryGetValue(word, out var translation))
                 {
                     words[i] = translation;
+                }
+                else
+                {
+                    words[i] = Transliterate(words[i]);
                 }
             }
             return string.Join(" ", words);
